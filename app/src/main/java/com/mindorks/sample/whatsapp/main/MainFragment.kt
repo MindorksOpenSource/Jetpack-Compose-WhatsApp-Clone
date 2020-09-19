a4c9cc5a58f3910289931b9410a4017147a038c1
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-09-19 20:35:41 +0530

    Migrate to TabView

--- app/src/main/java/com/mindorks/sample/whatsapp/main/MainFragment.kt
+++ app/src/main/java/com/mindorks/sample/whatsapp/main/MainFragment.kt
@@ -1,20 +1,23 @@
 package com.mindorks.sample.whatsapp.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import com.mindorks.sample.whatsapp.main.util.TabScreen
-import com.mindorks.sample.whatsapp.main.view.MainTabs
+import com.mindorks.sample.whatsapp.main.view.ScreenState
+import com.mindorks.sample.whatsapp.main.view.TabsPanel
 import com.mindorks.sample.whatsapp.ui.WhatsAppTheme
 
 class MainFragment : Fragment() {
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,12 +28,35 @@ class MainFragment : Fragment() {
 
             setContent {
                 WhatsAppTheme {
-                    var tabSelected =  remember { mutableStateOf(TabScreen.Chat) }
+                    onScreenSelected(
+                        ScreenState(ScreenState.Screen.CALLS),
+                        {
 
-                    MainTabs(titles = TabScreen.values().map { it.name }, tabSelected = TabScreen.Chat , onTabSelected = {
-                        tabSelected.value = TabScreen.values()[it.ordinal]
+                        }
+                    )
 
-                    })
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun onScreenSelected(
+        screenState: ScreenState,
+        onNavigate: (ScreenState.Screen) -> Unit
+    ) {
+        Column {
+            TabsPanel(screenState, onNavigate)
+            Surface {
+                when (screenState.state) {
+                    ScreenState.Screen.CALLS -> {
+                    }
+                    ScreenState.Screen.CHATS -> {
+
+                    }
+                    ScreenState.Screen.STATUS -> {
+
+                    }
                 }
             }
         }
