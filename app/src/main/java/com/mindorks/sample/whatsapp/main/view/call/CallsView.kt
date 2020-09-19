c9812917791a230504d7d1f33488fdb52dd1b67f
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-09-19 20:54:40 +0530

    Migrate to TabView

--- app/src/main/java/com/mindorks/sample/whatsapp/main/view/call/CallsView.kt
+++ app/src/main/java/com/mindorks/sample/whatsapp/main/view/call/CallsView.kt
@@ -1,16 +1,13 @@
 package com.mindorks.sample.whatsapp.main.view.call
 
-import androidx.compose.foundation.ScrollableColumn
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.runtime.Composable
 import com.mindorks.sample.whatsapp.data.local.calls
 
 @Composable
 fun CallsView() {
-    ScrollableColumn {
-        LazyColumnFor(calls) { call ->
-            CallItem(call)
-        }
+    LazyColumnFor(calls) { call ->
+        CallItem(call)
+
     }
 }
