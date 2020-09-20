d94b1d4c46cc68366fc129c85a58fba3dfbd7b6e
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-09-20 14:26:46 +0530

    User View

--- app/src/main/java/com/mindorks/sample/whatsapp/ui/Theme.kt
+++ app/src/main/java/com/mindorks/sample/whatsapp/ui/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-        primary = purple200,
-        primaryVariant = purple700,
-        secondary = teal200
+        primary = primaryWhatsAppColor,
+        primaryVariant = primaryWhatsAppColor,
+        secondary = primaryWhatsAppColor
 )
 
 private val LightColorPalette = lightColors(
-        primary = purple500,
-        primaryVariant = purple700,
-        secondary = teal200
+        primary = primaryWhatsAppColor,
+        primaryVariant = primaryWhatsAppColor,
+        secondary = primaryWhatsAppColor
 
         /* Other default colors to override
     background = Color.White,
